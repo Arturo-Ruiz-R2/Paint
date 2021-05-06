f19e091196a843a936962b636968607680810a8a
a01750147 <A01750147@itesm.mx>
2021-05-06 11:52:35 -0500

    Documentacion

--- paint.py
+++ paint.py
@@ -9,21 +9,32 @@ Exercises
 5. Add width parameter.
 
 """
-import turtle
+#Codigo modificado por:
+#Autor: Arturo Cuauhtémoc Ruiz Leyva
+#Autor: Andrea Vianey Díaz Álvarez
+
+"""Se importan las librerías que se usan en el código"""
+import turtle       #Para usar las funciones de turtle 
 from turtle import *
 from freegames import vector
 
+
 def line(start, end):
-    "Draw line from start to end."
+    """
+    Draw line from start to end.
+    """
     up()
     goto(start.x, start.y)
     down()
-    goto(end.x, end.y)
+    goto(end.x,end.y)
+
 
 def square(start, end):
-    "Draw square from start to end."
+    """
+    Draw square from start to end.
+    """
     up()
-    goto(start.x, start.y)
+    goto(start.x,start.y)
     down()
     begin_fill()
 
@@ -33,16 +44,23 @@ def square(start, end):
 
     end_fill()
 
+
 def circle(start, end):
+    """
+    Dibuja un circulo con radio (end.x-start.x)/2
+    """
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
-    turtle.circle(end.x - start.x)
+    turtle.circle((end.x-start.x)/2)    
     end_fill()
 
+
 def rectangle(start, end):
-    "Draw rectangle from start to end."
+    """
+    Draw rectangle from start to end.
+    """
     up()
     goto(start.x, start.y)
     down()
@@ -56,8 +74,11 @@ def rectangle(start, end):
 
     end_fill()
 
+
 def triangle(start, end):
-    "Draw triangle from start to end."
+    """
+    Draw triangle from start to end.
+    """
     up()
     goto(start.x, start.y)
     down()
@@ -70,8 +91,11 @@ def triangle(start, end):
         left(90)
     end_fill()
 
+
 def tap(x, y):
-    "Store starting point or draw shape."
+    """
+    Store starting point or draw shape.
+    """
     start = state['start']
 
     if start is None:
@@ -82,24 +106,33 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+
 def store(key, value):
-    "Store value in state at key."
+    """
+    Store value in state at key.
+    """
     state[key] = value
 
+
 state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
-onscreenclick(tap)
-listen()
+setup(420, 420, 370, 0)     #Define la ubicacion de la pantalla 
+onscreenclick(tap)     
+listen()         #Escucha los eventos del teclado
 onkey(undo, 'u')
+
+#Define las teclas que cambian de color 
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('orange'), 'O')
+
+#Define la letras que cambian las figuras 
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
+
 done()
